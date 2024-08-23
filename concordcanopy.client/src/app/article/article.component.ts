f5ad024050b36c1175a44e8506b12c24bcdce849
David <daniel.david.surla@gmail.com>
2024-08-23 13:42:59 +1000

    updated typescript files for components: article and card to test REST-ful datum.

--- concordcanopy.client/src/app/article/article.component.ts
+++ concordcanopy.client/src/app/article/article.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http"
+import { Component, OnInit } from '@angular/core'
+
+// Interface for REST-ful data
+interface ArticleArtificer {
+  id: string
+  title: string
+  description: string
+  author: string
+  date: string
+}
+// ... Break-line spacing for readability
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrl: './article.component.css'
 })
-export class ArticleComponent {
+export class ArticleComponent implements OnInit {
+  public articles: ArticleArtificer[] = []
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getArticles
+  }
+
+  getArticles() {
+    this.http.get<ArticleArtificer[]>("/articles").subscribe(
+      result => this.articles = result,
+      error => console.error(error)
+    )
+  }
 }
--- concordcanopy.client/src/app/components/card/card.component.ts
+++ concordcanopy.client/src/app/components/card/card.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http"
+import { Component, OnInit } from '@angular/core'
+// ...
+
+interface CardArtificer {
+  id: string
+  title: string
+  description: string
+  media: string
+}
+// ...
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+  public cards: CardArtificer[] = []
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getCards
+  }
+
+  getCards() {
+    this.http.get<CardArtificer[]>("/cards").subscribe(
+      result => this.cards = result,
+      error => console.error(error)
+    )
+  }
 }
